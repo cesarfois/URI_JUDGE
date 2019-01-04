f3d5ef39b72bb9568ae2cd15edeac9ac78325622
César J. Fois <fois2010@gmail.com>
2019-01-04 16:42:09 -0200

    uri 2059

--- 1973.py
+++ 1973.py
@@ -7,6 +7,3 @@ for i in lista:
     sum = sum + i
 
 print(sum)
-
-1 3 5 7 11 13 16 19
-      2 4 6 10 12 15 18
---
+++ 2057.py
@@ -0,0 +1,7 @@
+hi, hv, f = map(int,input().split())
+
+
+res = (hi + hv + f) % 24
+
+
+print(res)
