50f9caead8bd1ad9b430926c4ed0907e79a097a5
César J. Fois <fois2010@gmail.com>
2019-01-06 23:34:39 -0200

    URI 1973

--- 1973.py
+++ 1973.py
@@ -1,9 +1,33 @@
+print('='*72)
+print('{0:=^72}'.format(' URI 1973 PYTHON'))
+print('='*72)
+print('{0:=^72}'.format(' Jornada nas Estrelas '))
+print('{0:=^72}'.format(' By César J. Fois '))
+print('='*72)
+print('{0:=^72}'.format(' fois2010@gmail.com '))
+print('{0:=^72}'.format(' https://cesarfois.github.io/ '))
+print('='*72)
+print('')
+
+
 n = int(input())
 
-lista = list(map(int,input().split()))
-sum = 0
-for i in lista:
-    i = i - 1
-    sum = sum + i
+l = list(map(int,input().split()))
+temp, res, i = 0, 0, 0
 
-print(sum)
+while i < n and (l[i] % 2 != 0):
+    if l[i] >= 1:
+        l[i] = l[i] - 1
+    i += 1
+    temp = i
+
+if temp != n:
+    temp = temp + 1
+    for e in reversed(range(temp)):
+            if l[e] >= 1:
+               l[e] = l[e] - 1
+
+for w in range(n):
+    res = res + l[w]
+
+print(temp, res)
---
+++ GABI.py
@@ -0,0 +1,12 @@
+print('=============================================')
+print('+++++++ GABI TESTE SUA MATEMATICA +++++++++++')
+print('=============================================')
+
+
+
+n = input('GABI QUANTO É 3 + 3 ? \n')
+
+if int(n) == 6:
+    print("BOA GABI")
+else:
+    print('OHHH!! GABI BURRINHA')
